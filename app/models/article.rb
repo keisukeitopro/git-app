8668ffdf07374f907649d63ac8b4058a22b87adf
keisukeitopro <keisuke.ito.programmer@gmail.com>
2019-08-26 14:22:32 +0900

    add validation for article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,12 @@
+# 👇Bさん
 class Article < ApplicationRecord
   validates_presence_of(:title)
 end
+
+# 👇Aさん
+# class Article < ApplicationRecord
+#   validates_presence_of(:title)
+# end
 
 # class Article < ApplicationRecord
 # end
